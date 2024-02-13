261a08a6547565b71d7669cf7c2812d8f6238574
Prakhar Pathak <prakharpathak9450@gmail.com>
2024-02-13 20:09:09 +0530

    Added design

--- src/App.jsx
+++ src/App.jsx
@@ -19,6 +19,28 @@ function App() {
           <label>Password Length</label>
           <input type="number" max="20" />
         </div>
+
+        <div className="passLength">
+          <label>Include Uppercase Letters</label>
+          <input type="checkbox" />
+        </div>
+
+        <div className="passLength">
+          <label>Include Lowercase Letters</label>
+          <input type="checkbox" />
+        </div>
+
+        <div className="passLength">
+          <label>Include Numbers</label>
+          <input type="checkbox" />
+        </div>
+
+        <div className="passLength">
+          <label>Include Special Characters</label>
+          <input type="checkbox" />
+        </div>
+
+        <button className="btn">Generate Password</button>
       </div>
     </>
   );
