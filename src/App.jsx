2d0f6290afeaa5289371f32723b6e23b3d0c50ed
Prakhar Pathak <prakharpathak9450@gmail.com>
2024-02-17 18:38:06 +0530

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,23 @@
-import { useState } from 'react';
-import { Navbar } from './components/Navbar';
-import { Input } from './components/Input';
-import './App.css';
-import {GoogleGenerativeAI} from "@google/generative-ai";
-import ResultDisplay from './components/ResultsDisplay';
+import { useState } from "react";
+import { Navbar } from "./components/Navbar";
+import { Input } from "./components/Input";
+import "./App.css";
+import { GoogleGenerativeAI } from "@google/generative-ai";
+import ResultDisplay from "./components/ResultsDisplay";
 
 function App() {
-  const [response, setResponse] = useState('');
-  const [searchParams, setSearchParams] = useState({ location: '', purpose: '' });
+  const [response, setResponse] = useState("");
+  const [searchParams, setSearchParams] = useState({
+    location: "",
+    purpose: "",
+  });
 
-  const api_key = "apni api key daal";  
-  const genAI = new GoogleGenerativeAI(api_key);
+  const genAI = new GoogleGenerativeAI(import.meta.env.VITE_API_KEY);
 
   async function run() {
     // Use searchParams.location and searchParams.purpose in your prompt
     const prompt = `Generate a list of top four scholarships/grants/schemes available for student  in ${searchParams.location}. Include information on eligibility criteria, application deadlines,amount ,  and any specific requirements related to ${searchParams.purpose}. Provide direct links to the official webpages of each scholarship to facilitate easy access for prospective applicants. The scholarships should encompass various fields and academic levels, ensuring a diverse array of opportunities for students.`;
-    console.log(prompt)
+    console.log(prompt);
     const model = genAI.getGenerativeModel({ model: "gemini-pro" });
     const result = await model.generateContent(prompt);
     const response = await result.response;
@@ -31,11 +33,10 @@ function App() {
   return (
     <>
       <Navbar />
-      <div className='flex'>
-      <Input onSearch={handleSearch} />
-      <ResultDisplay response={response} />
+      <div className="flex">
+        <Input onSearch={handleSearch} />
+        <ResultDisplay response={response} />
       </div>
-      
     </>
   );
 }
